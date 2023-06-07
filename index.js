6064b331d2ddf15e5c3100f99047baee629a392f
vishal <vishrrajput02@gmail.com>
2023-06-07 16:16:04 +0530

    express installed and created server using it and used .json method

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-import http from "http"
-import generateLovePercent from "./features.js";
-import fs from "fs"
+import express from "express";
 
-const home = fs.readFileSync("./index.html")
+const app = express();
 
-const server = http.createServer((req,res)=>{   
-  if (req.url === "/about"){
-    res.end(`<h1>About ${generateLovePercent()}</h1>`)
-  }
-  else if (req.url === "/contact"){
-    res.end('<h1>Contact</h1>t</h1>')
-  }
-  else if (req.url === "/"){
-    res.end(home)
-  }
-  else{
-    res.end('<h1>Page Not Found</h1>')
-  }
- })
+app.get("/getproducts",(req,res)=>{
+  res.json({
+    success: true,
+    product: []
+  });
+});
 
-server.listen(5000,()=>{
-  console.log("server is working")
+app.listen(5000,()=>{
+  console.log('server is working');
 })
